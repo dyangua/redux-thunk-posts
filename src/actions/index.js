803c643b63dbdf2ee2937be4a113935ad639b78f
dyangua <dannyyanguacorrea@hotmail.com>
2019-08-18 21:55:16 -0500

    refactor users reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ export const fetchPosts = () => async dispatch => {
 };
 
 export const fetchUser = id => async dispatch => {
-  const response = await JsonPlaceHolder.get(`/users/1`);
+  const response = await JsonPlaceHolder.get(`/users/${id}`);
   dispatch({
     type: FETCH_USER,
     payload: response.data
--- src/components/User.js
+++ src/components/User.js
@@ -4,22 +4,29 @@ import { fetchUser } from "../actions/index";
 
 class User extends React.Component {
   componentDidMount() {
-    this.props.fetchUser(this.props.id);
+    this.props.fetchUser(this.props.userId);
   }
+
   render() {
+    const { user } = this.props;
     return (
       <div>
         <h6> AUTHOR INFO </h6>
-        <strong>{this.props.user.username}</strong>
-        <em> {this.props.user.email} </em>
+        {user && (
+          <div>
+            <strong>{user.username}</strong>
+            <em> {user.email} </em>
+          </div>
+        )}
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
+  const userPost = state.users.find(user => user.id === ownProps.userId);
   return {
-    user: state.user
+    user: userPost
   };
 };
 
